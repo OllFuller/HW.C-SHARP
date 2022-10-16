c1593641d09a9c403dae4a852dc98fcb3eb2c8ac
OLMAN <ollfuller@icloud.com>
2022-10-16 19:47:42 +0300

    My HomeWork

---
+++ Задача 2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.WriteLine("Вас, приветствует программа Min/Max");
+Console.WriteLine("Ввидете 2  числа");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int max,min;
+
+if (a > b)
+{
+    max =a;
+}
+else
+{
+    max = b;
+}
+if (a < b)
+{
+   min = a; 
+}
+else
+{
+    min = b;
+}
+
+Console.WriteLine("Минимальное значение:" + min);
+Console.WriteLine("Максимальное значение:" + max );
---
+++ Задача 4/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+Console.WriteLine("Вас, приветствует программа Max Nunber");
+Console.WriteLine("Ввидете 3 числа");
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (b > c)
+{
+    max = b;
+}
+else 
+{
+    max = c;
+}
+if (c > a)
+{
+    max = c;
+}
+else 
+{
+    a = max;
+}
+Console.WriteLine("Mаксимальное значение:" + max);
