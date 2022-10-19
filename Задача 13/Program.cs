1c8b3057616e6d0afad94eb3f6f65f0ed1f0796b
OllFuller <98952424+OllFuller@users.noreply.github.com>
2022-10-20 01:25:41 +0300

    My HomeWork

---
+++ Задача 13/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 13: Напишите программу, которая выводит
+третью цифру (справа налево) заданного числа или
+сообщает, что третьей цифры нет.
+645 -> 6
+78 -> третьей цифры нет
+32679 -> 6 
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число!");
+
+int number = int.Parse(Console.ReadLine()!);
+int third = number / 100 % 10;
+int result = third;
+if (third < number)
+{
+    Console.WriteLine("-> третьей цифры нет");
+}
+Console.WriteLine($"третья цифра в числе {number} -> {result}");
+
+
