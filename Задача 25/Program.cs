4c708d127c928ef76fb57c862ec4caf980369f07
OllFuller <98952424+OllFuller@users.noreply.github.com>
2022-11-08 17:49:39 +0300

    HWL4
    HWL4

---
+++ Задача 25/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B. Реализовать функцию возведения в степень
+самостоятельно!
+*/
+
+int GetNum(int a, int b)
+{
+    
+	int result = a;
+	for (int i = 0; i < b; i++)
+	{
+		result *= a;
+	}
+	return result;
+}
+
+
+Console.Clear();
+
+Console.Write("Введите чсло A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите чсло B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetNum(a,b));
