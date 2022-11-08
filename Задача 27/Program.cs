f111296358944c2ed98e800d3cb956cef88b9ff6
OllFuller <98952424+OllFuller@users.noreply.github.com>
2022-11-08 17:50:17 +0300

    HWL4

---
+++ Задача 27/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 27: Напишите функцию и запустите ее, которая
+принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+int sumDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите целое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(sumDigits(b));
