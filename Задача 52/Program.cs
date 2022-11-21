ec628765d79a461f5a3536be0accb74135f53e04
OllFuller <98952424+OllFuller@users.noreply.github.com>
2022-11-21 16:56:26 +0300

    HWL7

---
+++ Задача 52/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+Задача 52: Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом
+столбце.
+*/
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SquareEvenIndex(int[,] onArray)
+{
+
+    
+    
+    
+    
+
+   for (int i = 0; i < onArray.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < onArray.GetLength(0); j++)
+        {
+            sum += onArray[j, i];
+            
+
+        }
+
+        
+        Console.WriteLine($"среднее арефмитическое: {0}-го =  {sum / onArray.GetLength(1)}");
+
+    }
+
+
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+Console.WriteLine("************************");
+SquareEvenIndex(array);
